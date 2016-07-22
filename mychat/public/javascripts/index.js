2c5154519a36095177c67fa652307d7274beae45
kindboy <2515418556@qq.com>
2016-07-23 00:47:58 +0800

    laste refresh

--- mychat/public/javascripts/index.js
+++ mychat/public/javascripts/index.js
@@ -4,34 +4,59 @@ $(document).ready(function() {
         var itemName = $(this).attr('title');
         var itemImg = $(this).find('img').attr('src');
 
-        if (itemName === 'QQ') { msgP = 0;
-            minP = false; }
-        $('#bottom-ul li h2').text(itemName);
-        $('#bottom-ul img').attr('src', itemImg);
-        $('#bottom-ul').show();
-
-        if ($(this).next('div').attr('id') === 'qq') {
-            moveWindow($(this).next('div').attr('id'), $(this).next('div').attr('id'));
-
+        if (itemName === 'QQ') {
+            msgP = 0;
+            minP = false;
         }
-        if ($(this).next('div').attr('id') === 'photo') {
-            moveWindow($(this).next('div').attr('id'), $(this).next('div').attr('id'));
-        }
-        if ($(this).next('div').attr('id') === 'baidu') {
-            moveWindow($(this).next('div').attr('id'), $(this).next('div').attr('id'));
 
+        $('#bottom-ul li h2').first().text(itemName);
+        $('#bottom-ul img').first().attr('src', itemImg);
+        // $('#bottom-ul').show();
+
+        var bottomAppend = $('#bottom-ul li').first().html();
+        if(itemName === 'QQ'){
+            $("#bottom-ul li").remove('.QQ');
+            $('#bottom-ul').append("<li class='QQ'>"+bottomAppend+"</li>");
         }
+        if(itemName === 'Photo'){
+            $("#bottom-ul li").remove('.Photo');
+            $('#bottom-ul').append("<li class='Photo'>"+bottomAppend+"</li>");
+        }
+        if(itemName === 'Baidu'){
+            $("#bottom-ul li").remove('.Baidu');
+            $('#bottom-ul').append("<li class='Baidu'>"+bottomAppend+"</li>");
+        }
+
+        for (var i = 1; i < $('#bottom-ul li').length; i++) {
+            $('#bottom-ul li').eq(i).css('display', 'inline-block');
+        }
+
+        $('.window').css({
+            zIndex: '5'
+        });
+        $(this).next('div').css({
+            zIndex: '6'
+        });
+
+        moveWindow($(this).next('div').attr('id'), $(this).next('div').attr('id'));
 
         var offsetLeft = $(this).offset().left;
 
-        $(this).next('div').show().children('div').show().width(0).height(0);
-        $(".title-frame").animate({ width: "100%" ,height:"100%" }, 300);
+        $(this).next('div').show().children('div').show().width(0);
+        $(".title-frame").animate({ width: "100%", height: "26px" }, 300);
         $(".window-frame").animate({ width: "100%", height: "100%" }, 300);
     });
     $('.close-window').click(function() {
         $(this).parent().parent().hide();
-
-        $('#bottom-ul').hide();
+        if($(this).parent().attr('title') === 'QQ'){
+            $("#bottom-ul li").remove('.QQ');
+        }
+        if($(this).parent().attr('title') === 'Photo'){
+            $("#bottom-ul li").remove('.Photo');
+        }
+        if($(this).parent().attr('title') === 'Baidu'){
+            $("#bottom-ul li").remove('.Baidu');
+        }
     });
 
 
@@ -53,6 +78,31 @@ $(document).ready(function() {
             removeCenter(g($(this).parent().parent().attr('id')));
         }
     });
+
+
+    //动态为每个li添加点击事件
+    $('#bottom-ul').delegate('li', 'click',function() {
+        var temp = $(this).find('h2').text();
+        var show = function(id) {
+            document.getElementById(id).style.display = 'block';
+        };
+        msgP = 0;
+        $('.window').css({
+            zIndex: '5'
+        });
+        $(this).css({
+            zIndex: '6'
+        });
+        if (temp === 'QQ') { show('qq'); $('#qq').css({
+            zIndex: '6'
+        });}
+        if (temp === 'Photo') { show('photo'); $('photo').css({
+            zIndex: '6'
+        });}
+        if (temp === 'Baidu') { show('baidu'); $('baidu').css({
+            zIndex: '6'
+        });}
+    });
 });
 
 
@@ -67,16 +117,20 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('daytime').innerHTML = h + ":" + m + ":" + s;
 
-    if (msgP != 0) {
-        g('tip').innerHTML = msgP;
-        g('tip').style.background = 'red';
-    }else{
-        g('tip').innerHTML = '';
-        g('tip').style.background = 'transparent';
+    if (msgP !== 0) {
+        $('.QQ').find('p').text(msgP);
+        $('.QQ').find('p').css('background', 'red');
+        // g('tip').innerHTML = msgP;
+        // g('tip').style.background = 'red';
+    } else {
+        $('.QQ').find('p').text('');
+        $('.QQ').find('p').css('background', 'transparent');
+        // g('tip').innerHTML = '';
+        // g('tip').style.background = 'transparent';
     }
 
 
-    setTimeout('startTime()', 500);
+    setTimeout(function(){startTime();}, 500);
 }
 
 function checkTime(i) {
@@ -117,6 +171,12 @@ var isDraging = false;
 function moveWindow(windowTitle, window, isDraging) {
     //监听鼠标在标题栏上按下的事件
     g(windowTitle).addEventListener('mousedown', function(e) {
+        $('.window').css({
+            zIndex: '5'
+        });
+        $(this).css({
+            zIndex: '6'
+        });
         var e = e || window.evet;
         console.log(isDraging);
         mouseOffsetX = e.pageX - g(window).offsetLeft;
@@ -129,7 +189,7 @@ function moveWindow(windowTitle, window, isDraging) {
         document.getElementById('baidumask').style.display = 'block';
     });
 
-    g(window).onmousemove = function(e) {
+    document.addEventListener('mousemove', function(e) {
         var e = e || window.event;
         var mouseX = e.pageX;
         var mouseY = e.pageY;
@@ -156,13 +216,13 @@ function moveWindow(windowTitle, window, isDraging) {
             g(window).style.left = moveX + 'px';
             g(window).style.top = moveY + 'px';
         }
-    };
-    g(window).onmouseup = function() {
+    });
+    document.addEventListener('mouseup', function() {
         isDraging = false;
         document.getElementById('qqmask').style.display = 'none';
         document.getElementById('photomask').style.display = 'none';
         document.getElementById('baidumask').style.display = 'none';
-    };
+    });
 }
 
 var msgP = 0;
@@ -171,21 +231,17 @@ var minP = false;
 window.onload = function() {
     startTime();
 
-    g('min').onclick = function() {
-        minP = true;
-        g('qq').style.display = 'none';
-    };
-
-    g('bottom-ul').onclick = function(){
-        var temp=$('#bottom-ul li h2').text();
-        var show =function(id){
-            document.getElementById(id).style.display='block';
-        };
-        msgP = 0;
-        if(temp==='QQ'){show('qq');}
-        if(temp==='photo'){show('photo');}
-        if(temp==='baidu'){show('baidu');}
-    };
-
+    $('.min').click(function() {
+        if($(this).parent().attr('title') === 'QQ'){
+            minP = true;
+            g('qq').style.display = 'none';
+        }
+        if($(this).parent().attr('title') === 'Photo'){
+            g('photo').style.display = 'none';
+        }
+        if($(this).parent().attr('title') === 'Baidu'){
+            g('baidu').style.display = 'none';
+        }
+    });
 
 };
